43d266b181ab68dd8c406c3106c8353d30a1849c
Paul Clairmont <pcac25@gmail.com>
2015-02-17 16:17:09 -0500

    adds resources Inst and Stud as well as controllers as well as sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    # handle the lookup and auth
+    if(params[:title] == 'instructor')
+      instructor = Instructor.find_by({email: params[:email]})
+
+      if instructor && instructor.authenticate(params[:password])
+        session[:instructor_id] = instructor.id
+        # redirect_to '/home'
+      end
+    elsif(params[:title] == 'student')
+      student = Student.find_by({email: params[:email]})
+
+      if student && student.authenticate(params[:password])
+        session[:student_id] = student.id
+        # redirect_to '/home'
+      end
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+  end
+
+end
