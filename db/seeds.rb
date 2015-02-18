3a6481085009482dbc7858efa9a6fa47ecb5fb89
Paul Clairmont <pcac25@gmail.com>
2015-02-18 14:33:05 -0500

    updates schema, creates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.destroy_all
+Instructor.destroy_all
+Cohort.destroy_all
+Quiz.destroy_all
+Question.destroy_all
+Answer.destroy_all
+
+Student.create([
+  {name: "Bob", email: "bob@gmail.com", password: "cats"},
+  {name: "Billy", email: "billy@gmail.com", password: "cats"}
+  ])
+
+Instructor.create([
+  {name: "Phil", email: "phil@gmail.com", password: "cats"},
+  {name: "PJ", email: "pj@gmail.com", password: "cats"}
+  ])
+
+Cohort.create([
+  {name: "godot", instructor_id: 1, room: "3E", producer: "Sarah"}
+  ])
+
+Quiz.create([
+  {name: "Quiz 1", grade: 0, has_grade?: false},
+  {name: "Quiz 2", grade: 0, has_grade?: false},
+  ])
+
+Question.create([
+  {quiz_id: 1, content: "What is the best color?", got_right: false, points: 3}
+  ])
+
+Answer.create([
+  {question_id: 1, content: "green", is_picked: false}
+  ])
