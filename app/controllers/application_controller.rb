0f474782b5906d8c335bc1193ae98f04ccd6a97c
Paul Clairmont <pcac25@gmail.com>
2015-02-19 14:23:34 -0500

    makes changes to our models and our schema... could be deadly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # @student = Student.find(session[:student_id])
+
   def current_instructor
     @current_instructor ||= Instructor.find(session[:instructor_id]) if session[:instructor_id]
   end
--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,7 +1,11 @@
 class QuizController < ApplicationController
 
   def index
+    @student = Student.find(session[:student_id])
+    binding.pry
+    # @student.cohort.quizzes
     quiz_array = Quiz.where(been_taken: false)
+
     if(quiz_array == [])
       @response = "No quizzes, come back later"
     else
@@ -10,4 +14,9 @@ class QuizController < ApplicationController
     end
   end
 
+    # quiz_array.each do |q|
+    #   q.students.each do |s|
+    #     s.id == @student.id
+    #   end
+    # end
 end
