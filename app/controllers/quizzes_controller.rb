f2e570f6b0cb975a0c3ce95cc05623f35a98ace7
Andres Valencia <andresv2@mac.com>
2015-02-19 17:02:29 -0500

    adds an almost complete create quiz form

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,8 +1,18 @@
 class QuizzesController < ApplicationController
 
-def show
-  @quiz = Quiz.find(params[:id])
-end
+  def show
+    @quiz = Quiz.find(params[:id])
+  end
 
+  def create
 
+  end
+
+  def new
+    @quiz = Quiz.new
+    3.times do
+      question= @quiz.questions.build
+      4.times { question.answers.build }
+    end
+  end
 end
