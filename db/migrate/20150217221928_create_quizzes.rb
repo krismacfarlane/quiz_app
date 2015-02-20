e2a867f6ffbaf33b33df9b9422fa399653072380
Paul Clairmont <pcac25@gmail.com>
2015-02-20 11:10:26 -0500

    adds changes to migrations to match schema

--- db/migrate/20150217221928_create_quizzes.rb
+++ db/migrate/20150217221928_create_quizzes.rb
@@ -1,9 +1,12 @@
 class CreateQuizzes < ActiveRecord::Migration
   def change
     create_table :quizzes do |t|
-       t.string  'name',           null: false
-       t.integer 'grade',          :default => 0
-       t.boolean 'has_grade',      :default => false
+       t.string     'name',           null: false
+       t.integer    'grade',          :default => 0
+       t.boolean    'has_grade',      :default => false
+       t.boolean    'been_taken',     :default => false
+       t.references 'students'
+       t.references 'cohorts'
        t.timestamps
     end
   end
--- db/migrate/20150218144519_create_student_quiz.rb
+++
@@ -1,8 +0,0 @@
-class CreateStudentQuiz < ActiveRecord::Migration
-  def change
-    create_table :student_quizzes do |t|
-      t.references :students
-      t.references :quizzes
-    end
-  end
-end
