0cb2c2563a367c20b5749f23193b42f03c2ae6d7
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-02-21 17:06:56 -0500

    allows new instructors to create a new cohort

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -3,4 +3,19 @@ class CohortsController < ApplicationController
   def show
     @cohort = Cohort.find(params[:id])
   end
+
+  def new
+    @cohort = Cohort.new
+    @instructor_id = session[:instructor_id]
+  end
+
+  def create
+    @cohort = Cohort.create(cohort_params)
+    redirect_to cohort_path(@cohort)
+  end
+
+  def cohort_params
+    params.require(:cohort).permit(:name, :instructor_id, :room, :producer)
+  end
+
 end
